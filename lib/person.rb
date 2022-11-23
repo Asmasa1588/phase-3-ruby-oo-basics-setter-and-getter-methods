a17716fb3ef49ee208a8c1a7424cf775328cde72
Asma <as1588ma@gmail.com>
2022-11-23 12:06:32 -0500

    complete homework

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
+    def name=(nameArg)
+        @name = nameArg
+    
+    end
+    
+    def name
+        @name
+    end
+    
+    def job=(jobArg)
+        @job= jobArg
+    end
+    
+    def job
+        @job
+    end
 end
